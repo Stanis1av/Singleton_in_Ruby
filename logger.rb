4ea9814d564bdaec81b7829efb1b67c8c9116859
Stanis1av <krasavin.stanislaus@gmail.com>
2020-11-17 21:44:58 +0300

    using Singleton to ruby

--- logger.rb
+++ logger.rb
@@ -1,23 +1,14 @@
+require 'singleton'
+
 class Logger
+  include Singleton
+
   def initialize
     @f = File.open('log.txt', 'a')
   end
 
-  @@x = Logger.new
-
-  def self.instance
-    return @@x
-  end
-
-  # class method
-  def self.say_something
-    puts 'it method say something 1'
-  end
-
-  # instance mehod
   def log_something wat
     @f.puts wat
+    @f.flush
   end
-
-  private_class_method :new
 end
